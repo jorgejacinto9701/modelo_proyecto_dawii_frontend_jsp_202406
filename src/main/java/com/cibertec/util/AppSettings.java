39634d004aba5c99fdcc1bf51f912394a0d2a8a1
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-11-12 01:15:07 -0500

    ajustes

--- src/main/java/com/cibertec/util/AppSettings.java
+++ src/main/java/com/cibertec/util/AppSettings.java
@@ -2,8 +2,8 @@ package com.cibertec.util;
 
 public class AppSettings {
 
-	//public static final String URL_BACKEND = "https://bountiful-enchantment-production.up.railway.app";
-	public static final String URL_BACKEND = "http://localhost:8090";
+	public static final String URL_BACKEND = "https://bountiful-enchantment-production.up.railway.app";
+	//public static final String URL_BACKEND = "http://localhost:8090";
 	
 	
 
